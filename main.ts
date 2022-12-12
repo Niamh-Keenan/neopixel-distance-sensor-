2b1a2496859505535e7f4ea4ee8ddf85aadc93ac
Niamh-Keenan <113034875+Niamh-Keenan@users.noreply.github.com>
2022-12-12 09:52:04 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,35 +1,58 @@
 let distance_to_object = 0
-let strip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
-strip.clear()
+let strip: neopixel.Strip = null
 basic.showIcon(IconNames.Yes)
 basic.pause(500)
 basic.forever(function () {
+    strip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
     distance_to_object = sonar.ping(
     DigitalPin.P1,
     DigitalPin.P2,
     PingUnit.Centimeters
     )
     basic.showNumber(distance_to_object)
-    if (distance_to_object > 4) {
+    if (distance_to_object < 4) {
         strip.setPixelColor(0, neopixel.colors(NeoPixelColors.Green))
         strip.setPixelColor(1, neopixel.colors(NeoPixelColors.Green))
         strip.setPixelColor(2, neopixel.colors(NeoPixelColors.Green))
+        strip.setPixelColor(3, neopixel.colors(NeoPixelColors.Green))
         strip.show()
         strip.clear()
-        basic.pause(1000)
-    } else if (distance_to_object == 4) {
+    } else if (distance_to_object == 1) {
+        strip.setPixelColor(0, neopixel.colors(NeoPixelColors.Blue))
+        strip.show()
+        strip.clear()
+    } else if (distance_to_object == 1) {
+        strip.setPixelColor(0, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(1, neopixel.colors(NeoPixelColors.Blue))
+        strip.show()
+        strip.clear()
+    } else if (distance_to_object == 2) {
         strip.setPixelColor(0, neopixel.colors(NeoPixelColors.Blue))
         strip.setPixelColor(1, neopixel.colors(NeoPixelColors.Blue))
         strip.setPixelColor(2, neopixel.colors(NeoPixelColors.Blue))
         strip.show()
         strip.clear()
-        basic.pause(1000)
+    } else if (distance_to_object == 3) {
+        strip.setPixelColor(0, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(1, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(2, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(3, neopixel.colors(NeoPixelColors.Blue))
+        strip.show()
+        strip.clear()
+    } else if (distance_to_object == 4) {
+        strip.setPixelColor(0, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(1, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(2, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(3, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(4, neopixel.colors(NeoPixelColors.Blue))
+        strip.show()
+        strip.clear()
     } else if (distance_to_object < 1) {
         strip.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
         strip.setPixelColor(1, neopixel.colors(NeoPixelColors.Red))
         strip.setPixelColor(2, neopixel.colors(NeoPixelColors.Red))
+        strip.setPixelColor(3, neopixel.colors(NeoPixelColors.Red))
         strip.show()
         strip.clear()
-        basic.pause(1000)
     }
 })
